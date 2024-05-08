54976c4b2a74f3767058b642fbd20bfbb6c36393
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-05-08 17:55:53 -0400

    add Dashboard route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Dashboard from './routes/Dashboard.tsx'
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
     },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+    }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
